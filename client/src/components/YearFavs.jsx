e78cb3519c7a1d0c1f2f77b624e822c305b260a9
Chaymae El Hayani <layani.chay@gmail.com>
2024-07-18 15:34:37 +0200

    fix: updating year favorites component

--- client/src/components/YearFavs.jsx
+++ client/src/components/YearFavs.jsx
@@ -1,4 +1,5 @@
 import star from "../assets/images/star.png";
+import soonImage from "../assets/images/soon.jpg";
 
 export default function YearFavs() {
   return (
@@ -7,8 +8,8 @@ export default function YearFavs() {
         <img src={star} alt="Skincare" className="w-12 h-12 mr-6" /> 
         <span className="inline-block">2024 favorites</span>
       </h2>
-      <p className="text-Dark">Check out our top picks of the year !</p>
+      <p className="text-Dark">Check out our top picks of the year on December!</p>
+      <img src={soonImage} alt="Coming soon" className="w-full h-auto pt-9 rounded-lg mt-4" />
     </div>
   );
 };
-
